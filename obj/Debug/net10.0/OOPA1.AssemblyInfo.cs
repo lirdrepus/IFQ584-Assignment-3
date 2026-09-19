2f01086b1d4fd5f50a31df8e35e40a2d53cc2f70
Sean Grainger <ninqt@protonmail.com>
2026-09-19 17:16:10 +1000

    Example Rules Script

--- obj/Debug/net10.0/OOPA1.AssemblyInfo.cs
+++ obj/Debug/net10.0/OOPA1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOPA1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6462702f0113a9af6e7ca1bfdb8dd2966578ad1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bd92cb2ef53b7d72fbc0c91d0a2d7f77296e955")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOPA1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOPA1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
